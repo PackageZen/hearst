4b9030ad4784ff61902e1f127c2140e42ebe1a99
Daniel Mackey <mackey@packagezen.com>
2015-12-16 17:42:50 -0600

    Add stubbed specs for Hearst::Subscriber

---
+++ spec/subscriber_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Hearst::Subscriber do
+
+  it 'auto-registers itself with Hearst'
+  it 'validates required parameters'
+  it 'stores event and exchange configuration on class'
+  it 'raises error if .process is not implemented'
+
+end
